a99fc1ff44270b4b70511c4a01e094263a92b825
Rohan Kumar <rohan41878@gmail.com>
2026-08-16 22:50:34 +0530

    Remove unused testimonial section

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -2,7 +2,6 @@ import React from 'react'
 import Hero from '../components/Hero'
 import FeaturedSection from '../components/FeaturedSection'
 import Banner from '../components/Banner'
-import Testimonial from '../components/Testimonial'
 import Newsletter from '../components/Newsletter'
 
 const Home = () => {
@@ -11,7 +10,6 @@ const Home = () => {
       <Hero />
       <FeaturedSection />
       <Banner />
-      <Testimonial />
       <Newsletter />
     </>
   )
